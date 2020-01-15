93befb0bf727217fbec01f96e9a0dfd8c0402ccf
Jason <JSilvis030592@yahoo.com>
2020-01-15 13:26:49 -0500

    Bill calc

---
+++ new_Chal/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace new_Chal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diners = 4;
+            int cost = 10;
+            int bill = (diners * cost);
+            if (bill >= 50)
+            {
+                Console.WriteLine("You get a 10% discount!");
+            }
+            else 
+            {
+                Console.WriteLine("You get a 5% discount!"); 
+            }
+            
+        }
+    }
+}
